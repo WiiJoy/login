03572b80894bca09cbb4d81600ad516c64c42018
Vitaliy (work) <wiijoy@bk.ru>
2021-09-22 13:34:35 +0300

    Add notifications

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import UI from './config/ui.config';
 import {validate} from './helpers/validate';
 import {showInputError, removeInputError} from './views/form';
 import { login } from './services/auth.service';
+import { notify } from './views/notifications';
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -35,7 +36,12 @@ async function onSubmit() {
         await login(inputEmail.value, inputPassword.value);
         form.reset();
         // show success notify
+        // notify({ msg: 'Login success', className: 'alert-success', timeout: 1000 })
     } catch (error) {
         // show error notify
     }
 }
+
+notify({msg: 'Some notification 1', className: 'alert-danger'});
+notify({msg: 'Some notification 2', className: 'alert-warning'});
+notify({msg: 'Some notification 3', className: 'alert-primary'});
