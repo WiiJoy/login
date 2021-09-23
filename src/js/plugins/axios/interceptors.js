8ae9224e81a35a74026b6f375397de89825c2f4f
Vitaliy (work) <wiijoy@bk.ru>
2021-09-23 11:20:21 +0300

    Clear response and try request interceptors

--- src/js/plugins/axios/interceptors.js
+++ src/js/plugins/axios/interceptors.js
@@ -1,5 +1,16 @@
 const lsTokenKey = 'my_app_token';
 
+function setToken(req) {
+    const isAuthUrl = req.url.includes('auth');
+
+    if (!isAuthUrl) {
+        const token = localStorage.getItem(lsTokenKey);
+        req.headers['x-access-token'] = token;
+    }
+
+    return req;
+}
+
 
 function setTokenOnLogin(res) {
     const isLoginUrl = res.config.url.includes('login');
@@ -13,6 +24,13 @@ function setTokenOnLogin(res) {
     return res;
 }
 
+function getClearResponse(res) {
+    return res.data;
+}
+
 export default function (axios) {
+    axios.interceptors.request.use(setToken);
+
     axios.interceptors.response.use(setTokenOnLogin);
+    axios.interceptors.response.use(getClearResponse);
 }
